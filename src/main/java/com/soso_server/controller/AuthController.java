2a1b1a4906e8e210513ff4c70bdc5c18d8953507
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 15:36:15 +0900

    auth 검증 추가

---
+++ src/main/java/com/soso_server/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.soso_server.controller;
+
+import com.soso_server.dto.KakaoDTO;
+import com.soso_server.dto.MemberDTO;
+import com.soso_server.service.itf.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+public class AuthController {
+
+    /**
+     * authCheck구현
+     */
+    @PostMapping("/auth")
+    public boolean checkAuth(@RequestBody String authCode){
+        try {
+            System.out.println("AuthController.checkAuth");
+            if(authCode.equals("970917")){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+
+}
