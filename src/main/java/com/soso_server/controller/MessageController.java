c15547af06d96c1ec5b256d3be8c03e659e5f1b5
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-09-24 21:13:57 +0900

    firebase 추가

--- src/main/java/com/soso_server/controller/MessageController.java
+++ src/main/java/com/soso_server/controller/MessageController.java
@@ -1,5 +1,9 @@
 package com.soso_server.controller;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.soso_server.service.itf.MessageService;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
+@CrossOrigin(origins = "https://plater.kr:8888") // 허용할 오리진을 지정
 public class MessageController {
 
     private static final Logger logger = Logger.getLogger(MessageController.class);
@@ -41,4 +49,38 @@ public class MessageController {
             return new ResponseEntity<String>("-999", HttpStatus.BAD_REQUEST);
         }
     }
+
+    @Autowired
+    private FirebaseConfig firebaseConfig;
+
+    @PostMapping("/notification")
+    public String notificationTest(@RequestBody String token) throws FirebaseMessagingException {
+        testNoti(token);
+        return "알림전송..";
+    }
+
+    public void testNoti(String token) throws FirebaseMessagingException {
+        try {
+            // 7초(7000 밀리초) 동안 딜레이
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            // InterruptedException이 발생하면 예외 처리 코드를 작성할 수 있습니다.
+            e.printStackTrace();
+        }
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"); // 날짜 및 시간 형식 지정
+
+        Message message = Message.builder()
+                .setToken(token)
+                .setNotification(
+                        Notification.builder()
+                                .setTitle("알림 테스트...").setBody("전송시간[" + now.format(formatter)+"]")
+                                .build()
+                )
+                .build();
+
+        // 알림 보내기
+        String response = FirebaseMessaging.getInstance().send(message);
+    }
+
 }
